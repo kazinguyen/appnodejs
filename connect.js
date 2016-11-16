2496ee66e6ca0530683495a2753cc22dbd0e31de
kazinguyen <kazinguyen@kazinguyen.local>
2016-11-16 23:52:46 +0700

    remove file

--- connect.js
+++ connect.js
@@ -1,9 +1,9 @@
 var sql = require("mysql");
 var connection = sql.createConnection({
-    host        : '103.18.6.60',
-    user        : 'vietna1p_admin',
-    password    : 'kazinguyen',
-    database    : 'vietna1p_shop'
+    host        : '',
+    user        : '',
+    password    : '',
+    database    : ''
 });
 
 connection.connect(function(err) {
